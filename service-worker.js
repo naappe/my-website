61c768383f09d8db9eeccae56487a7b1330f113f
naappe <naappe@gmail.com>
2026-08-31 10:57:12 +0500

    Reset site for new website

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE="inbox-agenda-v1",ASSETS=["./","index.html","web/style.css","web/app.js","web/config.js","manifest.webmanifest","icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET"||new URL(e.request.url).origin!==location.origin)return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))});
--- web/config.js
+++
@@ -1,3 +0,0 @@
-window.APP_CONFIG = {
-  googleClientId: "REPLACE_WITH_WEB_OAUTH_CLIENT_ID.apps.googleusercontent.com"
-};
