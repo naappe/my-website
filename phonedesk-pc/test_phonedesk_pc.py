3a342b3b38dd477495051195c885bebae207866d
naappe <naappe@gmail.com>
2026-09-14 10:53:30 +0500

    chore: bring Windows PhoneDesk v0.2 tests into remote branch

---
+++ phonedesk-pc/test_phonedesk_pc.py
@@ -0,0 +1,55 @@
+import unittest
+
+import phonedesk_pc
+
+
+class PhoneDeskCoreTests(unittest.TestCase):
+    def test_make_endpoint(self):
+        self.assertEqual(phonedesk_pc.make_endpoint("192.168.1.10", "43211"), "192.168.1.10:43211")
+
+    def test_make_endpoint_rejects_bad_port(self):
+        with self.assertRaises(ValueError):
+            phonedesk_pc.make_endpoint("192.168.1.10", "99999")
+
+    def test_pairing_code_requires_six_digits(self):
+        self.assertEqual(phonedesk_pc.validate_pairing_code("123456"), "123456")
+        for bad in ("12345", "1234567", "12A456", ""):
+            with self.assertRaises(ValueError):
+                phonedesk_pc.validate_pairing_code(bad)
+
+    def test_parse_adb_devices(self):
+        output = """List of devices attached
+192.168.1.10:43211 device product:foo model:bar transport_id:1
+emulator-5554 offline transport_id:2
+"""
+        self.assertEqual(
+            phonedesk_pc.parse_adb_devices(output),
+            [("192.168.1.10:43211", "device"), ("emulator-5554", "offline")],
+        )
+
+    def test_parse_mdns_services_finds_pairing_and_connect_endpoints(self):
+        output = """List of discovered mdns services
+adb-ABC123._adb-tls-pairing._tcp. 192.168.1.55:37123
+adb-ABC123._adb-tls-connect._tcp. 192.168.1.55:40421
+"""
+        self.assertEqual(
+            phonedesk_pc.parse_mdns_services(output),
+            {"pairing": ("192.168.1.55", 37123), "connect": ("192.168.1.55", 40421)},
+        )
+
+    def test_parse_mdns_services_accepts_service_names_without_trailing_dot(self):
+        output = "adb-X _adb-tls-pairing._tcp 10.0.0.5:37899"
+        found = phonedesk_pc.parse_mdns_services(output)
+        self.assertEqual(found["pairing"], ("10.0.0.5", 37899))
+
+    def test_parse_mdns_services_returns_empty_when_nothing_found(self):
+        self.assertEqual(phonedesk_pc.parse_mdns_services("List of discovered mdns services"), {})
+
+    def test_saved_config_schema_excludes_pairing_secret(self):
+        keys = {"host", "device_port", "screen_off"}
+        self.assertNotIn("pairing_code", keys)
+        self.assertNotIn("pairing_port", keys)
+
+
+if __name__ == "__main__":
+    unittest.main()
